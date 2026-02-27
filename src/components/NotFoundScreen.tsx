5c344e491e10d92a9a2eac3dcfaac62ae3d29097
Sanjivani Bhongade <70435997+sanju200@users.noreply.github.com>
2026-02-27 13:04:18 +0530

    added page not found screen

--- src/components/NotFoundScreen.tsx
+++ src/components/NotFoundScreen.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
-import { useAppNavigation } from '../../App';
+import { useAppNavigation } from '../context/AppContext';
 
 const NotFoundScreen = () => {
   const { navigate } = useAppNavigation();
